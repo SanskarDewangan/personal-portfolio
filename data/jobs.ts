3545c42074a4422eb4159b9f751575c806d0def9
Sanskar Dewangan <d.sanskar014@gmail.com>
2025-08-27 15:03:59 +0530

    jobs-updated

--- data/jobs.ts
+++ data/jobs.ts
@@ -1,14 +1,17 @@
+import { url } from "inspector";
 import { IJobs } from "../typings";
 
 // Array of job objects for the portfolio
 export const jobs: IJobs[] = [
   {
     id: "1",
-    company: "Weave",
-    designation: "Senior Software Engineer",
-    from: "2025-06-01",
-    companyUrl: "https://weave.com",
-    companyLinkedin: "https://linkedin.com/company/weave"
+    company: "Accenture Solutions Private Limited",
+    designation: "Associate Software Engineer",
+    from: "2025-02-21",
+    to: "2026-08-01", // remove if still working
+    logo: { url: "/images/accenture.png" }, // optional
+    companyUrl: "https://www.accenture.com/in-en",
+    companyLinkedin: "https://www.linkedin.com/company/accentureindia/posts/?feedView=all"
   },
   
 ];

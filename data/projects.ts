a6bfbe4d6787337d928698a617db39f09b6b2fac
Sanskar Dewangan <d.sanskar014@gmail.com>
2025-08-29 22:09:19 +0530

    Project-section updated

--- data/projects.ts
+++ data/projects.ts
@@ -1,15 +1,6 @@
 // Array of project objects for the portfolio
 export const projects = [
   { id: "1",
-    title: "Healthcare Management App",
-    uniqueId: 101,
-    description: "Developed using Next.js and Appwrite as backend, helps users to make appointments",
-    demoLink: "",
-    githubLink: "https://github.com/SanskarDewangan/Healthcare-Management-App",
-    techStack: [{ text: "NextJs" },{text: "TypeScript"},{text: "Appwrite"},{text: "Shadcn"},{text: "Tailwind CSS"},{text: "Twilio"}],
-    image: { url: "/projects/healthcare.png" }
-  },
-  { id: "2",
     title: "Expense Tracker",
     uniqueId: 102,
     description: "Developed an Expense tracker to help users monitor and manage their daily expenses.",
@@ -18,7 +9,7 @@ export const projects = [
     techStack: [{ text: "NextJs" },{text: "Tailwind CSS"},{text: "Drizzle ORM"},{text: "JavaScript"},{text: "Clerk"},{text: "Shadcn"}],
     image: { url: "/projects/expense.png" }
   },
-  { id: "5",
+  { id: "2",
     title: "Todo App",
     uniqueId: 105,
     description: "Developed using React.js and Tailwind CSS that allows users to add, update, and delete tasks also helps users to organized and manage the task.",
@@ -27,7 +18,7 @@ export const projects = [
     techStack: [{ text: "React" },{ text: "Tailwind CSS" },{ text: "JavaScript" }],
     image: { url: "/projects/todo.png" }
   },
-  { id: "6",
+  { id: "3",
     title: "Password Generator",
     uniqueId: 106,
     description: "A password generator app that creates strong, secure passwords. Users can customize length and character types, ensuring safety for online accounts.",
@@ -36,16 +27,7 @@ export const projects = [
     techStack: [{ text: "React" },{ text: "CSS" },{ text: "JavaScript" }],
     image: { url: "/projects/password_generator.jpg" }
   },
-  { id: "7",
-    title: "Gemini Clone",
-    uniqueId: 107,
-    description: "Developed Gemini clone using React.js and Tailwind CSS and integrated Gemini API for searching the task",
-    demoLink: "https://gemini-clone-navy-six.vercel.app/",
-    githubLink: "https://github.com/SanskarDewangan/Gemini-Clone",
-    techStack: [{ text: "React" },{ text: "Tailwind CSS" },{ text: "JavaScript" }],
-    image: { url: "/projects/gemini.jpg" }
-  },
-  { id: "3",
+  { id: "4",
     title: "Grocery Store",
     uniqueId: 103,
     description: "CLI based grocery store wriiten in C++ which prints bill after the shopping and uses File Handling Concepts to store and retrieve data.",
@@ -54,7 +36,7 @@ export const projects = [
     techStack: [{ text: "C++" }],
     image: { url: "/projects/grocery.webp" }
   },
-  { id: "4",
+  { id: "5",
     title: "Library Management",
     uniqueId: 104,
     description: "A C++ Library Management System that enables adding, issuing, returning, and removing books, includes separate user and librarian menus.",
